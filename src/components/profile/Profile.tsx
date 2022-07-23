8f84c03f7ceb6fb607363ff01dd59bedcba13ed7
Egor <nullatnodes@icloud.com>
2022-07-23 21:24:57 +0700

    Introduce profile component

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,20 +1,18 @@
 import React, {useEffect} from "react";
-import Telegrams from "./telegrams/Telegrams";
 import HorizontalLine from "../helpers/HorizontalLine";
 
-const Settings = () => {
+const Profile = () => {
     useEffect(() => {
     }, [])
 
     return (
 
         <div className="min-h-screen flex flex-col items-center">
-            <p className={'text-md mt-5'}>Telegram Notifications</p>
-            <Telegrams></Telegrams>
+            <p className={'text-md mt-5'}>Profile</p>
             <HorizontalLine color={'text-amber-100'} className={'w-1/3 border rounded-full mt-10'} />
             <p className={'text-md mt-5'}>Under construction</p>
         </div>
     );
 }
 
-export default Settings;
+export default Profile;
