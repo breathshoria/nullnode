c9419017384565cc014dbcd08e04e4adce8103b8
Egor <nullatnodes@icloud.com>
2022-08-05 03:00:43 +0700

    Make mobile menu fixed

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -49,7 +49,7 @@ const Nav = () => {
         <Disclosure as="nav" className="bg-gray-900 rounded-b-lg">
             <div className="max-w-full mx-auto px-2 sm:px-6 lg:px-8">
                 <div
-                    className="hidden transition-opacity duration-200 opacity-0 ease-in-out absolute top-14 left-1 sm:invisible"
+                    className="hidden fixed z-20 transition-opacity duration-200 opacity-0 ease-in-out absolute top-14 left-1 sm:invisible"
                     ref={menuDropdown}>
                     <div className="flex flex-col p-2 border w-56 h-56 shadow-black shadow-sm rounded-b-md border-gray-900 bg-gray-900">
                         {navigation
