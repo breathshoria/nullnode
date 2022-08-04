e993d7ef02860ca38b6d6fd4be5f2921df5f9ea7
Egor <nullatnodes@icloud.com>
2022-08-05 05:37:50 +0700

    Improve login and register fonts

--- src/components/authentication/Register.tsx
+++ src/components/authentication/Register.tsx
@@ -24,12 +24,12 @@ const Register = () => {
         await auth?.registerUser(credentials, () => navigate('/'))
     }
     return (
-        <div className={'min-h-screen pb-12 flex flex-1 flex-col items-center justify-center'}>
+        <div className={'min-h-screen mt-5 flex flex-col items-center'}>
             {auth?.error && <Error error={auth.error}></Error>}
             <form className={'w-2/3 sm:w-1/3'}>
                 <div className={'pb-2'}>
                     <label>
-                        <span className={'text-xl sm:text-base'}>Username</span>
+                        <span className={'text-lg sm:text-base'}>Username</span>
                         <input
                             className={'mt-3 h-10 pl-2 bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-1'}
                             type="text"
@@ -41,7 +41,7 @@ const Register = () => {
                 </div>
                 <div className={'pt-2 pb-2'}>
                     <label>
-                        <span className={'text-xl sm:text-base'}>E-mail</span>
+                        <span className={'text-lg sm:text-base'}>E-mail</span>
                         <input
                             className={'mt-3 h-10 pl-2 bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-1'}
                             type="text"
@@ -53,7 +53,7 @@ const Register = () => {
                 </div>
                 <div className={'pt-2 pb-2'}>
                     <label>
-                        <span className={'text-xl sm:text-base'}>Password</span>
+                        <span className={'text-lg sm:text-base'}>Password</span>
                         <input
                             className={'mt-3 h-10 pl-2 bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'}
                             value={credentials.password}
@@ -68,7 +68,7 @@ const Register = () => {
                     onClick={handleSubmit}
                 >
                     {auth?.isLoading && <Loader className={'w-5 h-5'}></Loader>}
-                    <span className={'text-xl sm:text-base'}>Register</span>
+                    <span className={'text-lg sm:text-base'}>Register</span>
                 </button>
             </form>
         </div>
