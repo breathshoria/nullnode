521d8762e43507aa1288ab2fdb30178e60d6e8cd
Egor <nullatnodes@icloud.com>
2022-08-06 04:44:40 +0700

    Reorganize main project component

--- src/components/projects/Project.tsx
+++ src/components/projects/Project.tsx
@@ -6,7 +6,16 @@ import Loader from "../helpers/Loader";
 interface Project {
     title: string;
     description: string;
+    stage: string;
     logo: string;
+    website: string;
+    discord: string;
+    github: string;
+    telegram: string;
+    summary: string;
+    startDate: string;
+    involvement: string;
+    guide: string;
 }
 
 const Project = () => {
@@ -40,23 +49,43 @@ const Project = () => {
 
     return (
         <>
-            <div className={'min-h-screen p-2 w-3/4 flex flex-col gap-4 items-center mx-auto'}>
+            <div className={'min-h-screen p-5 w-full flex flex-col gap-4 items-center mx-auto'}>
                 <div
-                    className={'bg-sky-900 rounded-lg w-full p-2 flex flex-col justify-center sm:flex-row items-center'}>
-                    <img className={'w-20 ml-3 inline-block'}
-                         src={`${process.env.REACT_APP_API_DOMAIN}/${project.logo}`}/>
-                    <div className={'grow text-center'}>
-                        <span className={'text-4xl sm:text-5xl font-medium font-mono'}>{project.title}</span>
+                    className={'border-sky-800 border-2 rounded-lg w-3/4 p-2 flex flex-col justify-evenly sm:flex-row sm:flex-wrap items-center'}>
+                    <img
+                        className={'w-20 ml-3 inline-block'}
+                        src={`${process.env.REACT_APP_API_DOMAIN}/${project.logo}`}
+                    />
+                    <div className={'text-center'}>
+                        <span className={'text-4xl sm:text-5xl font-medium'}>{project.title}</span>
+                    </div>
+                    <div className={'flex flex-col items-center gap-2 p-2 font-medium text-base text-black sm:text-lg'}>
+                        <div className={'bg-white rounded-md p-2 w-full'}>
+                            <p className={'text-center'}>{project.stage}</p>
+                        </div>
+                        <div className={'bg-white rounded-md p-2 w-full flex flex-row gap-2'}>
+                            <a href={project.website}><img className={'fill-sky-700 w-5'} src={'/website.svg'}/></a>
+                            <a href={project.github}><img className={'fill-sky-700 w-5'} src={'/github.svg'}/></a>
+                            <a href={project.discord}><img className={'fill-sky-700 w-5'} src={'/discord.svg'}/></a>
+                            <a href={project.telegram}><img className={'fill-sky-700 w-5'} src={'/telegram.svg'}/></a>
+                        </div>
                     </div>
                 </div>
-                <div className={'mt-2 grid sm:grid-cols-2 gap-5 w-full min-h-screen'}>
-                    <div className={'bg-blue-700'}>
-                        <span>content</span>
+                <div className={'mt-2 grid sm:grid-cols-2 gap-5 w-3/4'}>
+                    <div className={'border-sky-800 border-2 rounded-md p-4 flex flex-col'}>
+                        <p className={'text-center font-medium text-lg'}>Summary</p>
+                        <p className={'text-base'}>{project.summary}</p>
                     </div>
-                    <div className={'bg-blue-900'}>
-                        <span>content2</span>
+                    <div className={'border-sky-800 border-2 rounded-md p-4 flex flex-col'}>
+                        <p className={'text-center font-medium text-lg'}>Participation</p>
+                        <p>{project.involvement}</p>
                     </div>
-
+                </div>
+                <div className={'mt-2 flex flex-col w-3/4'}>
+                    <div className={'border-sky-800 border-2 rounded-md p-4 flex flex-col'}>
+                        <h1 className={'text-center font-medium text-lg'}>Helpful information</h1>
+                        <p>{project.guide}</p>
+                    </div>
                 </div>
             </div>
         </>
